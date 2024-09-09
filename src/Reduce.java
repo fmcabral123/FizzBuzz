94177e93458175e19fb9b0ec8eb214caf0f91295
Franco Cabral <francomaricabral@gmail.com>
2024-09-09 18:20:43 -0400

    added Reduce class

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+        while (n != 0){
+            if (n % 2 == 0){
+                n = n / 2;
+            } else if (n % 2 != 0){
+                n = n - 1;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
 }
